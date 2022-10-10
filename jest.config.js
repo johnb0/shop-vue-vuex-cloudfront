ca288f4b17cf5a733bcc165baac39e38f056ec12
Andrey Korolev <andrey.korolev@kingfisher.com>
2022-10-10 18:54:10 +0200

    feat: tests cov

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
 	preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
 	setupFiles: ['./jest.setup.js'],
+	collectCoverageFrom: [
+		'src/**/*.{js,jsx,ts,tsx}',
+		'!**/*.d.ts',
+		'!**/node_modules/**',
+		'!**/__mocks__/**',
+	],
+	coveragePathIgnorePatterns: [
+		'<rootDir>/node_modules/',
+		'<rootDir>/coverage/',
+		'<rootDir>/jest.config.js',
+		'<rootDir>/jest.setup.js',
+	],
+	coverageDirectory: './coverage',
 };
